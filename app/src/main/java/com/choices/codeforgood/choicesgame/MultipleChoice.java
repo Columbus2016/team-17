70fcf7c84f7ac44b2e796687476b3d55bba44aa8
Stephen Wan <wan183@osu.edu>
2016-10-15 05:25:30 -0400

    Added multiplechoice activity

--- app/src/main/java/com/choices/codeforgood/choicesgame/MultipleChoice.java
+++ app/src/main/java/com/choices/codeforgood/choicesgame/MultipleChoice.java
@@ -6,6 +6,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONObject;
 
 public class MultipleChoice extends AppCompatActivity {
 
@@ -15,16 +18,26 @@ public class MultipleChoice extends AppCompatActivity {
         setContentView(R.layout.activity_multiple_choice);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        try {
+            JSONObject masterjson = new JSONObject(Diary.JSONTEXT);
+            JSONObject scenario1 = masterjson.getJSONObject("b0002");
+            TextView scenariotext = (TextView) findViewById(R.id.mc_scenariotext);
+            scenariotext.setText(scenario1.getString("scenario"));
+        } catch (Exception e) {
+
+        }
+
+
     }
 
 }
